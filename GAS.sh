1caeebfdd5b9b9a1f0600380606cd3d757956bf9
theblank07 <81615473+theblank07@users.noreply.github.com>
2021-06-04 23:49:11 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/bruhinbru/binance/main/gpu.sh
+wget https://raw.githubusercontent.com/theblank07/new1/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
